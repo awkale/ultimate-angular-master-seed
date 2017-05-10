c0b2005e406484b00d8b39bf6017bffb78afd68f
Alex Kale <alex.kale@prnewswire.com>
2017-05-10 15:20:30 -0400

    add sidebar

---
+++ src/app/common/app-sidebar.controller.js
@@ -0,0 +1,22 @@
+function AppSidebarController() {
+  this.contactTags = [{
+    label: 'All contacts',
+    icon: 'star'
+  }, {
+    label: 'Friends',
+    icon: 'people'
+  }, {
+    label: 'Family',
+    icon: 'child_care'
+  }, {
+    label: 'Acquaintances',
+    icon: 'accessibility'
+  }, {
+    label: 'Following',
+    icon: 'remove_red_eye'
+  }];
+}
+
+angular
+  .module('common')
+  .controller('AppSidebarController', AppSidebarController);
