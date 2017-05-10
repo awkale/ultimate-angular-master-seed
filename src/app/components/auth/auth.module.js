bcdc69e7936acfc2bdcf173a9ab6df103ee33985
Alex Kale <hi@awkale.me>
2017-05-09 22:17:01 -0400

    hookup firebase

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -3,3 +3,21 @@ angular
     'ui.router',
     'firebase'
   ]);
+.config(function($firebaseRefProvider) {
+  var config = {
+    apiKey: "AIzaSyAXgsvhWC9XcDCOQHQjr9fzPryiiWx1e7k",
+    authDomain: "contacts-manager-eb093.firebaseapp.com",
+    databaseURL: "https://contacts-manager-eb093.firebaseio.com",
+    projectId: "contacts-manager-eb093",
+    storageBucket: "contacts-manager-eb093.appspot.com",
+    messagingSenderId: "717770883211"
+  };
+
+  $firebaseRefProvider
+    .registerUrl({
+      default: config.databaseURL,
+      contacts: config.databaseURL + '/contacts'
+    });
+
+  firebase.initializeApp(config);
+});
