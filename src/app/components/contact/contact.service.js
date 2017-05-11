a1a884f32b1ac172ae360aa6e17972823b8df0b8
Alex Kale <hi@awkale.me>
2017-05-10 21:56:36 -0400

    new contact service

---
+++ src/app/components/contact/contact.service.js
@@ -0,0 +1,13 @@
+Function ContactService(AuthService, $firebaseRef, $firebaseArray) {
+  let ref = $firebaseRef.contacts;
+  let uid = AuthService.getUser().uid;
+  return {
+    createNewContact: (contact) => {
+      return $firebaseArray(ref.child(uid)).$add(contact);
+    }
+  };
+}
+
+angular
+  .module('components.contact')
+  .factory('ContactService', ContactService);
