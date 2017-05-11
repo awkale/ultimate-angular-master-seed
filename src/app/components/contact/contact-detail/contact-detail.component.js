ae1973f1be7e09d1e46ccf466ee0b90292f50348
Alex Kale <hi@awkale.me>
2017-05-10 22:23:06 -0400

    add contact detail component

---
+++ src/app/components/contact/contact-detail/contact-detail.component.js
@@ -0,0 +1,12 @@
+let contactDetail = {
+  bindings: {
+    contact: '<',
+    onSave: '&'
+  },
+  templateUrl: './contact-detail.html',
+  controller: 'ContactDetailController'
+};
+
+angular
+  .module('components.contact')
+  .component('contactDetail', contactDetail);
---
+++ src/app/components/contact/contact-detail/contact-detail.controller.js
@@ -0,0 +1,16 @@
+Function ContactDetailController() {
+  this.$onInit = () => {
+    this.isNewContact = !this.contact.$id;
+  };
+  this.saveContact = () => {
+    this.onSave({
+      $event: {
+        contact: this.contact
+      }
+    });
+  };
+}
+
+angular
+  .module('components.contact')
+  .controller('ContactDetailController', ContactDetailController);
